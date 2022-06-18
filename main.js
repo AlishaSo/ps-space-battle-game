d2e4b347edd1bbec6bb3990d354ceb165718a691
Alisha Soriano <sorianoalisha@gmail.com>
2022-06-18 13:51:33 -0400

    Create an attack method that determines whether the ships hit each other or not, and what happens in each scenario

--- main.js
+++ main.js
@@ -5,4 +5,31 @@ class Ship {
     this.firepower = firepower;
     this.accuracy = accuracy;
   }
+
+  attack(attackee) {
+    const hero = this.name == 'hero';
+    if(this.accuracy > attackee.accuracy) {
+      attackee.hull -= this.firepower;
+      if(hero) {
+        console.log(`You Hit an alien! 💪🏽`);
+      }
+      else
+        console.log('The alien has hit You 😖');
+      console.log(`You have ${hero ? 'done' : 'taken'} ${this.firepower} damage`);
+      console.log(`${!hero ? 'You have' : 'The alien has'} ${attackee.hull} left`);
+    }
+    else {
+      if(hero)
+        console.log('You have Missed the alien 🙃');
+      else
+        console.log('The alien has missed You! 🙌🏽');
+    }
+  }
 }
+
+// const hero = new Ship(20, 5, .7, 'hero');
+// const alien = new Ship(Math.floor(Math.random() * (6 - 3 + 1) + 3), Math.floor(Math.random() * (4 - 2 + 1) + 2), Number((Math.random() * (.8-.6) + .6).toFixed(1)))
+
+// console.log(hero, alien);
+// hero.attack(alien);
+// alien.attack(hero);
